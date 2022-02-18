972295efc99a81e1b64adc32d8b3b3210eb51649
200920082007 <93928612+200920082007@users.noreply.github.com>
2022-02-18 12:07:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/200920082007/TigerShroff.git /TigerShroff
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax
+  git clone $UPSTREAM_REPO /Tigershroff
 fi
-cd /Ajax
+cd /Tigershroff
 pip3 install -U -r requirements.txt
 echo "Starting TIGER Shroff....🔥"
 python3 bot.py
