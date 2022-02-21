e1b0abc12cfb72a8ea3aebcc6e85d2b74348322e
200920082007 <93928612+200920082007@users.noreply.github.com>
2022-02-21 18:38:50 +0530

    Update vars.py

--- Alita/vars.py
+++ Alita/vars.py
@@ -1,33 +1,30 @@
-from os import getcwd
+from os import environ
 
-from prettyconf import Configuration
-from prettyconf.loaders import EnvFile, Environment
 
-env_file = f"{getcwd()}/.env"
-config = Configuration(loaders=[Environment(), EnvFile(filename=env_file)])
-
+def load_var(var_name, def_value=None):
+    return environ.get(var_name, def_value)
 
 class Config:
     """Config class for variables."""
 
     LOGGER = True
-    BOT_TOKEN = config("BOT_TOKEN", default=None)
-    APP_ID = int(config("APP_ID", default=None))
-    API_HASH = config("API_HASH", default=None)
-    OWNER_ID = int(config("OWNER_ID", default=1198820588))
-    MESSAGE_DUMP = int(config("MESSAGE_DUMP", default=-100))
-    DEV_USERS = [int(i) for i in config("DEV_USERS", default="").split()]
-    SUDO_USERS = [int(i) for i in config("SUDO_USERS", default="").split()]
-    WHITELIST_USERS = [int(i) for i in config("WHITELIST_USERS", default="").split()]
-    DB_URI = config("DB_URI", default="")
-    DB_NAME = config("DB_NAME", default="alita_robot")
-    NO_LOAD = config("NO_LOAD", default="").split()
-    PREFIX_HANDLER = config("PREFIX_HANDLER", default="/").split()
-    SUPPORT_GROUP = config("SUPPORT_GROUP", default="DivideProjectsDiscussion")
-    SUPPORT_CHANNEL = config("SUPPORT_CHANNEL", default="DivideProjects")
-    ENABLED_LOCALES = [str(i) for i in config("ENABLED_LOCALES", default="en").split()]
-    VERSION = config("VERSION", default="v2.0")
-    WORKERS = int(config("WORKERS", default=16))
+    BOT_TOKEN = load_var("BOT_TOKEN")
+    APP_ID = int(load_var("APP_ID"))
+    API_HASH = load_var("API_HASH")
+    OWNER_ID = int(load_var("OWNER_ID"))
+    MESSAGE_DUMP = int(load_var("MESSAGE_DUMP", -100))
+    DEV_USERS = [int(i) for i in load_var("DEV_USERS", "").split()]
+    SUDO_USERS = [int(i) for i in load_var("SUDO_USERS", "").split()]
+    WHITELIST_USERS = [int(i) for i in load_var("WHITELIST_USERS", "").split()]
+    DB_URI = load_var("DB_URI")
+    DB_NAME = load_var("DB_NAME", "alita_robot")
+    NO_LOAD = load_var("NO_LOAD", "").split()
+    PREFIX_HANDLER = load_var("PREFIX_HANDLER", "/").split()
+    SUPPORT_GROUP = load_var("SUPPORT_GROUP")
+    SUPPORT_CHANNEL = load_var("SUPPORT_CHANNEL")
+    ENABLED_LOCALES = [str(i) for i in load_var("ENABLED_LOCALES", "").split()]
+    VERSION = load_var("VERSION")
+    WORKERS = int(load_var("WORKERS", 16))
     BOT_USERNAME = ""
     BOT_ID = ""
     BOT_NAME = ""
