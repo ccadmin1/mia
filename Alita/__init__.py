60b7a2e08777b3879a88211520671eace359a0ff
200920082007 <93928612+200920082007@users.noreply.github.com>
2022-02-22 18:38:21 +0530

    Update __init__.py

--- Alita/__init__.py
+++ Alita/__init__.py
@@ -76,8 +76,8 @@ SUPPORT_STAFF = list(
 )  # Remove duplicates by using a set
 
 # Plugins, DB and Workers
-DB_URI = Config.DB_URI
-DB_NAME = Config.DB_NAME
+DATABASE_URI = Config.DB_URI
+DATABASE_NAME = Config.DB_NAME
 NO_LOAD = Config.NO_LOAD
 WORKERS = Config.WORKERS
 
