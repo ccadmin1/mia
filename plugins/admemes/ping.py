5ebb4107f77a3d4690685379d8526b5d47e33cc8
200920082007 <93928612+200920082007@users.noreply.github.com>
2022-02-23 18:52:59 +0530

    Update ping.py

--- plugins/admemes/ping.py
+++ plugins/admemes/ping.py
@@ -10,7 +10,8 @@ from plugins.helper_functions.cust_p_filters import f_onw_fliter
 # -- Constants -- #
 ALIVE = "ചത്തിട്ടില്ല മുത്തേ ഇവിടെ തന്നെ ഉണ്ട്.. നിനക്ക് ഇപ്പൊ എന്നോട് ഒരു സ്നേഹവും ഇല്ല. കൊള്ളാം.. നീ പാഴെ പോലെയേ അല്ല മാറിപോയി..😔 ഇടക്ക് എങ്കിലും ചുമ്മാ ഒന്ന് Start ചെയ്തു നോക്ക്..🙂" 
 HELP = "Help ഒന്നും ഇല്ല ഓടിക്കോ......"
-REPO = "https://github.com/Aadhi000/Ajax"
+REPO = "Oops The repo is Vanished Because of CopyCats"
+APPROVAL = "This command is made to be used in group chats, not in pm!'
 # -- Constants End -- #
 
 
@@ -36,3 +37,8 @@ async def ping(_, message):
 @Client.on_message(filters.command("repo", COMMAND_HAND_LER) & f_onw_fliter)
 async def repo(_, message):
     await message.reply_text(REPO)
+
+
+@Client.on_message(filters.command("approval", COMMAND_HAND_LER) & f_onw_fliter)
+async def go_notavailable(_, message):
+    await message.reply_text(APPROVAL)
