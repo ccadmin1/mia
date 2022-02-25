d1afa7d76667fc7e7d6d5f2adf085bea54f33dd2
200920082007 <93928612+200920082007@users.noreply.github.com>
2022-02-25 07:18:57 +0530

    Update notes.py

--- plugins/notes.py
+++ plugins/notes.py
@@ -5,14 +5,14 @@ from pyrogram import filters
 from pyrogram.errors import RPCError
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, Message
 
-from alita import LOGGER
-from alita.bot_class import Alita
-from alita.database.notes_db import Notes, NotesSettings
-from alita.utils.cmd_senders import send_cmd
-from alita.utils.custom_filters import admin_filter, command, owner_filter
-from alita.utils.kbhelpers import ikb
-from alita.utils.msg_types import Types, get_note_type
-from alita.utils.string import (
+from Alita import LOGGER
+from pyrogram import Client as Alita
+from database.notes_db import Notes, NotesSettings
+from Alita.utils.cmd_senders import send_cmd
+from Alita.utils.custom_filters import admin_filter, command, owner_filter
+from Alita.utils.kbhelpers import ikb
+from Alita.utils.msg_types import Types, get_note_type
+from Alita.utils.string import (
     build_keyboard,
     escape_mentions_using_curly_brackets,
     parse_button,
