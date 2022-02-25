2fae42f0a83a432421b9303ea2dd70acf637d14d
200920082007 <93928612+200920082007@users.noreply.github.com>
2022-02-25 16:44:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1518,7 +1518,7 @@ async def advantage_spell_chok(msg):
         )
     ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
-    await msg.reply("I couldn't find anything related to that\nDid you mean any one of these?",
+    await msg.reply("reply_sticker(sticker = "CAACAgQAAxkBAAJKwWIWM_NZGV1ziKPjnfSIg0E4mtrjAAJVAAONG0Yn7Ih7NKFLMJseBA")",
                     reply_markup=InlineKeyboardMarkup(btn))
 
 async def manual_filters(client, message, text=False):
