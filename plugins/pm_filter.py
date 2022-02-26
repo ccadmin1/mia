b2707b9050e379c3a2c038815fb7ef7aea1f8494
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-02-26 15:01:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1522,7 +1522,7 @@ async def advantage_spell_chok(msg):
     ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
     await msg.send_sticker(chat_id=message.from_user.id, sticker="CAACAgQAAxkBAAJQzmIZo8u3pHF8ulLUGT7dUeIjAAF78QACVQADjRtGJ-yIezShSzCbHgQ"),
-    reply_markup=InlineKeyboardMarkup(btn))
+    reply_markup=InlineKeyboardMarkup(btn)
 
 async def manual_filters(client, message, text=False):
     group_id = message.chat.id
