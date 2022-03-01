151b53489ef05097707b8b259830a551d5f0de46
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-03-01 19:25:43 +0530

    Update __init__.py

--- rose/__init__.py
+++ rose/__init__.py
@@ -22,6 +22,7 @@ from sys import exit as sysexit
 from traceback import format_exc
 import os
 import asyncio
+from info import FSUB_CHANNEL
 from pyrogram import Client
 from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
