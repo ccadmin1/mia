6eccb35197d5136027d48b6b76ae487cedc17d34
WARRIORS <56763972+ccadmin1@users.noreply.github.com>
2022-03-05 23:02:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/200920082007/TigerShroff.git /TigerShroff
+  git clone https://github.com/ccadmin1/mia /mia
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Tigershroff
 fi
 cd /Tigershroff
 pip3 install -U -r requirements.txt
-echo "Starting TIGER Shroff....🔥"
+echo "Starting mia....🔥"
 python3 bot.py
