35619fb90d658a37555ad6954bb835a82cb256ae
WARRIORS <56763972+ccadmin1@users.noreply.github.com>
2022-03-06 10:21:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,7 @@
 echo "Cloning Repo, Please Wait..."
-
-git clone -b master https://github.com/ccadmin1/mia /mia
-
-cd /mia
-
+git clone -b master https://github.com/ccadmin1/Movie-Bot /IMDb-Movie-Bot
+cd /IMDb-Movie-Bot
 echo "Installing Requirements..."
-
 pip3 install -U -r requirements.txt
-
 echo "Starting Bot, Please Wait..."
-
 python3 bot.py
